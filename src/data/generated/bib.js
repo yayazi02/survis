6e28fbaae7e85da0ad379f578f71a107e12e7c53
yayazi02 <yinyin719@outlook.com>
2025-05-16 08:12:23 +0800

    bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -8,7 +8,7 @@
     "journal": "Music Perception",
     "doi": "10.1525/mp.2006.24.1.49"
   },
-      "Jack2016": {
+     "Jack2016": {
     "title": "Effect of latency on performer interaction and subjective quality assessment of a digital musical instrument",
     "author": "Jack, R.H.; Stockman, T.; McPherson, A.P.",
     "year": "2016",
@@ -17,7 +17,7 @@
     "journal": "Audio Mostly",
     "doi": "10.1145/2986416.2986420"
   },
-  "Rottondi2016": {
+     "Rottondi2016": {
     "title": "An overview on networked music performance technologies",
     "author": "Rottondi, C.; Chafe, C.; Allocchio, C.; Sarti, A.",
     "year": "2016",
@@ -26,7 +26,7 @@
     "journal": "IEEE Access",
     "doi": "10.1109/ACCESS.2016.2628440"
   },
-  "Grimm2024": {
+     "Grimm2024": {
     "title": "Interactive low delay music and speech communication via network connections (OVBOX)",
     "author": "Grimm, G.",
     "year": "2024",
@@ -35,7 +35,7 @@
     "journal": "Acta Acustica",
     "doi": "10.1051/aacus/2024005"
   },
-  "Turchet2023": {
+     "Turchet2023": {
     "title": "Immersive networked music performance systems: identifying latency factors",
     "author": "Turchet, L.; Tomasetti, M.",
     "year": "2023",
@@ -44,16 +44,16 @@
     "journal": "I3DA",
     "url": "https://ebooks.iospress.nl/volumearticle/22374"
   },
- "GrimmWerner2021": {
-      "title": "Real-time music over IP: Audio quality and transmission delay trade-offs",
-      "author": "Grimm, G.; Werner, C.",
-      "year": "2021",
-      "type": "conference",
-      "keywords": "real-time audio, IP music, delay, quality",
-      "journal": "AES Conference",
-      "url": "https://www.aes.org/e-lib/browse.cfm?elib=21076"
+      "GrimmWerner2021": {
+     "title": "Real-time music over IP: Audio quality and transmission delay trade-offs",
+     "author": "Grimm, G.; Werner, C.",
+     "year": "2021",
+     "type": "conference",
+     "keywords": "real-time audio, IP music, delay, quality",
+     "journal": "AES Conference",
+     "url": "https://www.aes.org/e-lib/browse.cfm?elib=21076"
   },
-  "Chafe2010": {
+      "Chafe2010": {
     "title": "JackTrip: Under the hood of an engine for network audio",
     "author": "Chafe, C.; C\u00e1ceres, J.P.",
     "year": "2010",
@@ -62,7 +62,7 @@
     "journal": "Journal of New Music Research",
     "doi": "10.1080/09298215.2010.481361"
   },
-  "Carot2009": {
+     "Carot2009": {
     "title": "Network music performance\u2014Problems, approaches and perspectives",
     "author": "Car\u00f4t, A.; Werner, C.",
     "year": "2009",
@@ -71,16 +71,16 @@
     "journal": "AES 35th International Conference",
     "url": "https://www.aes.org/e-lib/browse.cfm?elib=15271"
   },
-  "Drioli2013": {
-  "title": "Networked performances and natural interaction via LOLA: Low latency high quality A/V streaming system",
-  "author": "Drioli, C.; Allocchio, C.",
-  "year": "2013",
-  "type": "bookchapter",
-  "keywords": "LOLA, streaming, performance",
-  "journal": "Information Technologies for Performing Arts, Media Access, and Entertainment",
-  "doi": "10.1007/978-3-642-40050-6_23"
+     "Drioli2013": {
+    "title": "Networked performances and natural interaction via LOLA: Low latency high quality A/V streaming system",
+    "author": "Drioli, C.; Allocchio, C.",
+    "year": "2013",
+    "type": "bookchapter",
+    "keywords": "LOLA, streaming, performance",
+    "journal": "Information Technologies for Performing Arts, Media Access, and Entertainment",
+    "doi": "10.1007/978-3-642-40050-6_23"
   },
-  "Caceres2010": {
+     "Caceres2010": {
     "title": "JackTrip: Under the hood of an engine for network audio",
     "author": "C\u00e1ceres, J.P.; Chafe, C.",
     "year": "2010",
